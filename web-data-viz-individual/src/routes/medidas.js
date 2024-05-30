4c14763c20b9dc16b7001c0493128da86ab10ec0
Pedro <pedro.fjesus@sptech.school>
2024-05-30 01:41:14 -0300

    Subindo modificações no Cadastro

--- web-data-viz-individual/src/routes/medidas.js
+++ web-data-viz-individual/src/routes/medidas.js
@@ -11,4 +11,8 @@ router.get("/tempo-real/:idAquario", function (req, res) {
     medidaController.buscarMedidasEmTempoReal(req, res);
 })
 
+router.get("/quiz/:idUsuario", function (req, res) {
+    medidaController.buscarMedidasEmTempoReal(req, res);
+})
+
 module.exports = router;
